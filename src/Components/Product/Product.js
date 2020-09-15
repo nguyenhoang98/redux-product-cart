f22befa1608421f7676461aea72a1d8ec677ec17
nguyenhoang98 <nguyenvanhoangg150998@gmail.com>
2020-09-15 16:54:56 +0700

    redux-product-cart

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
+import './Product.css' ;
 class Product extends Component {
+  renderRating = (rating)=>{
+     let xhtml = [] ;
+     for(var i = 1 ; i <= rating ; i++){
+       xhtml.push(<i key = {i} className="fa fa-star" aria-hidden="true"></i>)
+     }
+     for(var j = 1 ; j <= 5-rating ; j++){
+       xhtml.push(<i key = {j+rating} className="fa fa-star-o" aria-hidden="true"></i>)
+     }
+     return xhtml ;
+  }
   onAddProduct = (product, quantity) => {
     this.props.onAddProduct(product, quantity);
   };
@@ -12,7 +23,11 @@ class Product extends Component {
         </div>
         <div className="product-item-content">
           <div className="product-item-name">{product.name}</div>
-          <div className="product-item-rating"></div>
+          <div className="product-item-rating">
+            {
+              this.renderRating(product.rating)
+            }
+          </div>
           <div className="product-item-description">{product.description}</div>
           <div className="product-item-cart">
             <button
